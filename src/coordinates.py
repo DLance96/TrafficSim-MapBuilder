db2a401ed9cb2bb33578cbe61549e09785dfabca
AlexTryjan <aet41@case.edu>
2017-10-25 18:49:36 -0400

    object clicking implemented, simple road generation added, updated formatting of base files

--- src/coordinates.py
+++ src/coordinates.py
@@ -1,24 +1,25 @@
-#Class that stores coordinates of a point. 
+# Class that stores coordinates of a point.
 class coordinates:
+    # Constructor for coordinates. Stores x and y coord values of a point.
+    def __init__(self, x_val, y_val):
+        self.x = x_val
+        self.y = y_val
 
-	#Constructor for coordinates. Stores x and y coord values of a point. 
-	def __init__(self, x_val, y_val):
-		self.x = x_val
-		self.y = y_val
-		
-	#returns x coord value of a point. 
-	def get_x(self):
-		return self.x
-		
-	#returns y coord value of a point. 
-	def get_y(self):
-		return self.y
-	
-#main method for coordinates	
+    # returns x coord value of a point.
+    def get_x(self):
+        return self.x
+
+    # returns y coord value of a point.
+    def get_y(self):
+        return self.y
+
+
+# main method for coordinates
 def main():
-	coord = coordinates(2.1, 3.3)
-	print 'x: ' + str(coord.get_x())
-	print 'y: ' + str(coord.get_y())
-		
+    coord = coordinates(2.1, 3.3)
+    print('x: ' + str(coord.get_x()))
+    print('y: ' + str(coord.get_y()))
+
+
 if __name__ == '__main__':
-	main()
+    main()
