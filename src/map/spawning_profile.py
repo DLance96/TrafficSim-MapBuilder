fe5d9fcd0bb9ccc0e55870a7f58b97271a862357
David Lance <dcl71@case.edu>
2017-10-26 01:11:15 -0400

    Intial xml_report commit with basic exporting
    Signed-off-by: David Lance <dcl71@case.edu>

---
+++ src/map/spawning_profile.py
@@ -0,0 +1,5 @@
+class SpawningProfile(object):
+
+    def __init__(self, driver_profile, rate):
+        self.driver_profile = driver_profile
+        self.rate = rate
---
+++ src/xml/export.py
@@ -0,0 +1,108 @@
+import xml.etree.ElementTree as ET
+
+
+def export(roads, intersections, save_location):
+    """
+    Main function of export that makes that the map is valid and then creates an xml file
+    :param roads: list of the roads in the map
+    :param intersections: list of the roads in the map
+    :param save_location: where to save the xml file
+    :return:
+    """
+    if is_connected(roads, intersections):
+        make_xml(roads, intersections, save_location)
+    else:
+        return  # TODO decide what to do with incomplete map
+
+
+def make_xml(roads, intersections, save_location):
+    """
+    Creates an xml document from a map and saves it to a given location
+    :param roads: list of roads in the map
+    :param intersections: list of intersections in the mpa
+    :param save_location: location of the xml file
+    :return:
+    """
+    traffic_map = ET.Element("map")
+    road_elements = ET.SubElement(traffic_map, "roads")
+    intersection_elements = ET.SubElement(traffic_map, "intersections")
+
+    for index, road in enumerate(roads):
+        temp_road = ET.SubElement(road_elements, "road", name=index)
+        ET.SubElement(temp_road, "length").text = road.get_length()
+        ET.SubElement(temp_road, "incoming_lanes").text = road.get_in_lanes()
+        ET.SubElement(temp_road, "outgoing_lanes").text = road.get_out_lanes()
+
+        ET.SubElement(temp_road, "angle_radians").text = road.get_angle()
+        ET.SubElement(temp_road, "incoming_entrance_corner").text = ""
+        ET.SubElement(temp_road, "outgoing_entrance_corner").text = ""
+
+        ET.SubElement(temp_road, "speed_limit").text = ""  # TODO change when speed limit is added
+
+        if road.get_out_intersection() is not None:
+            ET.SubElement(temp_road, "outgoing_intersection").text = intersections.index(road.get_out_intersection())
+        if road.get_in_intersection() is not None:
+            ET.SubElement(temp_road, "incoming_intersection").text = intersections.index(road.get_in_intersection())
+
+    for index, intersection in enumerate(intersections):
+        temp_intersection = ET.SubElement(intersection_elements, "intersection", name=index)
+        ET.SubElement(temp_intersection, "center_point").text = intersection.get_center()
+        ET.SubElement(temp_intersection, "radius").text = intersection.get_radius()
+        temp_connections = ET.SubElement(temp_intersection, "connections")
+        for connected_road in intersection.get_connections:
+            ET.SubElement(temp_connections, "connection_road").text = roads.index(connected_road)
+
+    tree = ET.ElementTree(traffic_map)
+    tree.write(save_location)
+
+
+def is_connected(roads, intersections):
+    """
+    Verifies that a collection of intersections and roads is fully connected
+    :param roads: list of roads in the map
+    :param intersections: list of intersections in the map
+    :return: boolean on whether the map is fully connected
+    """
+    to_visit_roads = []
+    visited_roads = []
+    visited_intersections = []
+
+    if len(roads) == 0:
+        if len(intersections) == 0 or len(intersections) == 1:
+            return True
+        else:
+            return False
+
+    to_visit_roads.extend(roads.pop())
+
+    while len(to_visit_roads) > 0:
+        road = to_visit_roads.pop()
+        new_roads = []
+        if road.get_out_intersection() is not None and road.get_out_intersection() not in visited_intersections:
+            new_roads.extend(road.get_out_intersection().get_connections())
+            visited_intersections.extend(road.get_out_intersection())
+        if road.get_in_intersection() is not None and road.get_in_intersection() not in visited_intersections:
+            new_roads.extend(road.get_in_intersection().get_connections())
+            visited_intersections.extend(road.get_in_intersection())
+        remove_visited_roads(new_roads, visited_roads)
+
+    if len(roads) == len(visited_roads) and len(intersections) == len(visited_intersections):
+        return True
+    else:
+        return False
+
+
+def remove_visited_roads(roads, visited_roads):
+    """
+    Removes all the previous visted roads from the new roads list
+    :param roads: list of new roads to be filtered
+    :param visited_roads: list of roads that have already been visited
+    :return:
+    """
+    for road in roads:
+        if road in visited_roads:
+            roads.remove(roads)
+
+
+if __name__ == '__main__':
+    export([], [], "temp.xml")
