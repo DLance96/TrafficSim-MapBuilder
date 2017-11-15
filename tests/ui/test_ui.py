cf9a18674ea2198f951a1d832fea8c728a1f647c
AlexTryjan <aet41@case.edu>
2017-11-14 23:48:32 -0500

    AddObject UI
    Signed-off-by: AlexTryjan <aet41@case.edu>

---
+++ tests/ui/test_ui.py
@@ -0,0 +1,11 @@
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from src.map.Road import Road
+from src.map.Coordinates import Coordinates
+from src.map.Intersection import Intersection
+
+def test_
